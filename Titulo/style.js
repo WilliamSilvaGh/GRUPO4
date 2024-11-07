5c9af8f6d378508d0d8d17d96b47cb2a83b1457c
William Esquilino <williamsilvaesquilino@gmail.com>
2024-11-07 09:39:26 -0300

    Adicionado o component de título

---
+++ Titulo/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+ BoxTitle: {
+ alignItems: "center",
+ padding: 40,
+ backgroundColor: "#E8E8E8",
+ borderRadius: 10,
+ },
+
+ texTitle: {
+ color: "#1d72de",
+ fontWeight: "bold",
+ fontSize: 18,
+ },
+
+});
+
+export default styles
